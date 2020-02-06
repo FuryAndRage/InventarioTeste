b772f9e8b13dc438b03e6d8338fe22b6ee41bdc3
Moises <lawliex@gmail.com>
2020-02-06 19:31:13 +1300

    Adicionado Admin Fornecedor

--- Inventario/fornecedor/admin.py
+++ Inventario/fornecedor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fornecedor
 
-# Register your models here.
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ('nome_fornecedor', 'telefone_fornecedor')
+    search_fields = ('nome_fornecedor',)
