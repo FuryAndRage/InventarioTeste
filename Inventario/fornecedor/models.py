7b4f30149751f40524b5d494cd8d482a35cc7405
Moises <lawliex@gmail.com>
2020-02-06 17:00:10 +1300

    Adicionado model Fornecedor

--- Inventario/fornecedor/models.py
+++ Inventario/fornecedor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Fornecedor(models.Model):
+    nome_fornecedor = models.CharField('Fornecedor', max_length=50)
+    telefone_fornecedor = models.CharField(
+        'Telefone', max_length=50, blank=True)
+
+    def __str__(self):
+        return self.nome_fornecedor
